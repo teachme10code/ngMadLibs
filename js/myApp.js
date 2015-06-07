c85cee691ea97aa151ccfaedcf625bcf0ae30457
chris.ward <chris.ward@servicenow.com>
2015-06-06 22:32:03 -0700

    Form validation, submit button enabling on condition, conditionally hide/show divs, Form reset, and minor styling changes

--- js/myApp.js
+++ js/myApp.js
@@ -1,10 +1,17 @@
-//ngMadLibs refactor2
+//ngMadLibs refactor3
+
+"use strict";
 
 angular.module('myApp', [])
 	.controller('MadLibController', function($scope) {
 		$scope.showMadLibs = false;
 
-		$scope.submit = function() {
+		$scope.madLibValid = function() {
 			$scope.showMadLibs = true;
 		};
-	});
+
+		$scope.resetForm = function() {
+			$scope.showMadLibs = false;
+			mlForm.reset();
+		};
+	})
